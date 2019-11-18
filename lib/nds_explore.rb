c74802c5d78b1ec3aa33ee6a73bbba161d46ab90
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-18 17:09:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,12 @@
-require 'pp'
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  
+  require 'pp'
+  puts directors_database
 end
 
 def print_first_directors_movie_titles
